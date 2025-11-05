3997d9d1efd9013c815cf8e8dc86e80ec481437d
so-s1m4 <maks.rvachov.at@gmail.com>
2025-11-05 22:18:35 +0100

    sve

--- client/src/app/shared/utils/cleanObject.ts
+++ client/src/app/shared/utils/cleanObject.ts
@@ -2,7 +2,7 @@ export function cleanObject<T extends Record<string, any>>(obj: T): { [p: string
   return Object.fromEntries(
     Object.entries(obj)
       .filter(([_, v]) => {
-        if (v === null || v === undefined || v === '' || isNaN(v)) return false;
+        if (v === null || v === undefined || v === '' || (typeof v === 'number' && Number.isNaN(v))) return false;
         if (Array.isArray(v) && v.length === 0) return false;
         if (typeof v === 'object' && !Array.isArray(v) && Object.keys(v).length === 0) return false;
         return true;
