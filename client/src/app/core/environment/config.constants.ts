551b383084dacc86b86354c6c926a7fd57c0602f
so-s1m4 <maks.rvachov.at@gmail.com>
2025-12-29 11:15:09 +0100

    save

--- client/src/app/core/environment/config.constants.ts
+++ client/src/app/core/environment/config.constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:9011';
+export const API_URL = 'http://htlink.m0sk1tu4.codes';
 export const DEFAULT_AVATAR_URL = 'https://www.htlstp.ac.at/wp-content/uploads/2024/08/maus.jpeg';
 export const isDevMode = true;
