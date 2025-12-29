c76dbcd5edcd574f88b40ed839303c50b47e8445
so-s1m4 <maks.rvachov.at@gmail.com>
2025-12-29 21:09:23 +0100

    repulling

--- client/src/app/core/environment/config.constants.ts
+++ client/src/app/core/environment/config.constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://htlink.m0sk1tu4.codes';
+export const API_URL = 'https://htlink.m0sk1tu4.codes';
 export const DEFAULT_AVATAR_URL = 'https://www.htlstp.ac.at/wp-content/uploads/2024/08/maus.jpeg';
 export const isDevMode = true;
