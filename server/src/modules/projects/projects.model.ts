9702b8c9d2d49cc82d9e63d7200fd9dae5d01907
so-s1m4 <maks.rvachov.at@gmail.com>
2026-01-03 22:58:29 +0100

    back end

--- server/src/modules/projects/projects.model.ts
+++ server/src/modules/projects/projects.model.ts
@@ -1,80 +1,77 @@
-import { Model, Schema, model, Types, HydratedDocument } from "mongoose";
-import { ISkill } from "../skills/skills.model";
+import {HydratedDocument, model, Schema, Types} from "mongoose";
+import {ISkill} from "../skills/skills.model";
 
 export interface IProject {
-    _id: Types.ObjectId,
-    title: string,
-    categoryId: Types.ObjectId,
-    shortDescription: string,
-    fullReadme: string,
-    deadline?: Date,
-    createdAt: Date,
-    updatedAt: Date,
-    skills: ISkill[],
-    ownerId: Types.ObjectId,
-    status: ProjectStatus,
-    images: Types.ObjectId[],
+  _id: Types.ObjectId,
+  title: string,
+  categoryId: Types.ObjectId,
+  shortDescription: string,
+  fullReadme: string,
+  deadline?: Date,
+  createdAt: Date,
+  updatedAt: Date,
+  skills: ISkill[],
+  ownerId: Types.ObjectId,
+  status: ProjectStatus,
+  images: Types.ObjectId[],
 }
-
-
-
 
 
 export enum ProjectStatus {
-    PLANNED = "Planned",
-    IN_PROGRESS = "In progress",
-    COMPLETED = "Completed",
-    ON_HOLD = "On hold",
+  PLANNED = "Planned",
+  IN_PROGRESS = "In progress",
+  COMPLETED = "Completed",
+  ON_HOLD = "On hold",
 }
 
 export const projectSchema = new Schema<IProject>({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 30
-    },
-    categoryId: {
-        type: Schema.Types.ObjectId ,
-        ref: 'Category',
-    },
-    shortDescription: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 500
-    },
-    fullReadme: {
-        type: String,
-        required: false,
-    },
-    deadline: {
-        type: Date,
-        required: false
-    },
-    skills: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Skill',
-        required: false
-    }],
-    ownerId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    status: {
-        type: String,
-        enum: Object.values(ProjectStatus),
-        default: ProjectStatus.PLANNED
-    },
-    images: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Image',
-        required: false,
-        maxlength: 10
-    }], 
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 30
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+  shortDescription: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 500
+  },
+  fullReadme: {
+    type: String,
+    required: false,
+  },
+  deadline: {
+    type: Date,
+    required: false
+  },
+  skills: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Skill',
+    required: false
+  }],
+  ownerId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: Object.values(ProjectStatus),
+    default: ProjectStatus.PLANNED
+  },
+  images: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Image',
+    required: false,
+    maxlength: 10
+  }],
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 export const Project = model<IProject>("Project", projectSchema);
